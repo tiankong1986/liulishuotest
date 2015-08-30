212cebd81be0796b219c71c4352f120b5f686930
zhangyongmu <zhangyongmu@netfinworks.com>
2015-08-30 22:11:32 +0800

    打包命令

--- coins-txn-conf/mvn build.bat
+++ coins-txn-conf/mvn build.bat
@@ -4,4 +4,7 @@ cd %curdir%
 cd ../coins-txn-core
 call mvn -DM3_REP_HOME=E:\.m3\coins.txn.core.1.1.0 -f pom.xml clean install
 call mvn -DM3_REP_HOME=E:\.m3\coins.txn.core.1.1.0 -f pom.xml source:jar deploy
+
+cd ../coins-txn-api-ws
+call mvn -DM3_REP_HOME=E:\.m3\coins.txn.core.1.1.0 -f pom.xml clean package
 pause
